0a1a4479c160a99a854d7cf5d1e1c6b7a23c7873
saitarun999 <tarun.nuka@gmail.com>
2025-02-24 17:55:51 -0700

    music

--- Class_6/build.gradle.kts
+++ Class_6/build.gradle.kts
@@ -1,21 +1,5 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    kotlin("jvm") version "1.9.21"
-}
-
-group = "org.example"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-kotlin {
-    jvmToolchain(11)
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
 }
